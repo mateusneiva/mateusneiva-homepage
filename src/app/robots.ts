56c7c325a03667fc8770a75c30d36ca99b83bd66
Mateus Neiva <mateusneiva.dev@gmail.com>
2024-06-05 23:41:43 -0300

    feat: add metadata files

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://mateusneiva.vercel.app/sitemap.xml',
+  }
+}
