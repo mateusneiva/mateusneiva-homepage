6c1b8aab863ac26814bfd9f71a5d41534c8da969
Mateus Neiva <mateusneiva.dev@gmail.com>
2024-06-06 00:57:40 -0300

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://acme.com',
+      url: 'https://mateusneiva.vercel.app',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
