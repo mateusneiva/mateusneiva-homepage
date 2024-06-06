9092b7804c5f466348f04034af5fc07aba3b601a
Mateus Neiva <mateus.fneiva@gmail.com>
2024-06-06 17:21:02 -0300

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://mateusneiva.vercel.app/sitemap.xml',
+    sitemap: 'https://mateusneiva.com/sitemap.xml',
   }
 }
